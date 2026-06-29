e7278463724829103e8f1232deca0a26857f4664
Nandakishore P <nandakishorep212@gmail.com>
2026-06-30 00:48:48 +0530

    Updated Gradiet

--- tiny_llm/neuron.py
+++ tiny_llm/neuron.py
@@ -2,31 +2,31 @@ class Neuron:
     
     def __init__(self):
         self.w = 0.3
-        self.lr = 0.1
-        self.gradient = 3
+        self.lr = 0.0001
+        self.gradient = 1
+        self.epsilon = 0.0001
+        self.b = 0
     
     def predict(self, x):
-        return self.w * x
+        return self.w * x + self.b
     
     def loss(self, predicted, actual):
         return (actual-predicted) ** 2
     
+    def compute_gradient(self, actual_loss, predicted_loss):
+        return (predicted_loss - actual_loss) / self.epsilon
+
+    
     def train_step(self, x, y):
         loss_current = self.loss(self.predict(x), y)
-        current_w = self.w
-        self.w = current_w -  self.lr
-        loss_down = self.loss(self.predict(x), y)
-        self.w = current_w + self.lr
-        loss_up = self.loss(self.predict(x), y)
+        current_w = self.w 
+        self.w += self.epsilon
+        loss_new = self.loss(self.predict(x), y)
+        self.w = current_w
+        self.gradient = self.compute_gradient(loss_current, loss_new)
+        self.w -= self.lr * self.gradient
 
 
-        best = min(loss_current, loss_down, loss_up)
-        if best == loss_down:
-            self.w = current_w - self.lr
-        elif best == loss_up:
-            self.w = current_w + self.lr
-        else:
-            self.w = current_w
 data = [
     (1, 2),
     (2, 4),
@@ -46,4 +46,5 @@ for epoch in range(100):
         n.train_step(x, y)   
         total_loss += n.loss(n.predict(x), y)
 
-    print(epoch, n.w, total_loss)
+    if epoch % 10 == 0:
+        print(epoch, n.w, total_loss)
